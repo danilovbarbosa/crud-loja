8067341797ae6babe329533ec3ba7664cf9aa6d4
danilovbarbosa <danilo.v.barbosa@gmail.com>
2021-04-08 14:27:16 -0300

    feat(dao): definindos métodos (vázios) para inserir, remover e listar em CategoriaDAO e ProdutoDAO.

--- models/dao.py
+++ models/dao.py
@@ -1,8 +1,33 @@
 from models import models
 
 class CategoriaDAO:
-    pass
+    def __init__(self):
+        pass
 
+
+    def inserir(self): 
+        return 0
+
+
+    def remover(self):
+        return 0
+
+
+    def listar(self):
+        return 0
 
 class ProdutoDAO:
-    pass
+    def __init__(self):
+        pass
+
+
+    def inserir(self): 
+        return 0
+
+
+    def remover(self):
+        return 0
+
+
+    def listar(self):
+        return 0
