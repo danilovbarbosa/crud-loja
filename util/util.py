e7733aca5d7bd8f8272d8953c1f15cd6e3462670
danilovbarbosa <danilo.v.barbosa@gmail.com>
2021-04-08 14:23:42 -0300

    feat(util/Arquivo): definindos métodos para escrever e ler no arquivo.

--- util/util.py
+++ util/util.py
@@ -2,16 +2,30 @@ from pathlib import Path
 
 class Arquivo:
     @staticmethod
-    def escrever():
-        arquivo = open("nome-do-arquivo", 'r', encoding="utf8")    
+    def escrever(nome_do_arquivo, texto):
+        '''
+        >>> nome = "teste"
+        >>> escrever(nome)
 
+        '''
+        arquivo = Path(nome_do_arquivo).open('a')
+        arquivo.write(texto + '\n')
+        arquivo.close()
+        return arquivo 
 
     @staticmethod
-    def ler():
-        with q.open() as f: f.readline()
-        return 0    
+    def ler(nome_do_arquivo):
+        '''
+        >>> nome = "teste"
+        >>> escrever(nome, "teste")
+        >>> ler(nome)
+        teste
+        '''
+        arquivo = Path(nome_do_arquivo)
+        return arquivo.read_text()
 
     
     @staticmethod
     def remover():
         return 0    
+
