da5a8222ca7ba0242e22b680465c612db9c43a7a
danilovbarbosa <danilo.v.barbosa@gmail.com>
2021-04-08 13:23:03 -0300

    feat(models): inserindo métodos (vázios) para criar, remover e listar nas classes Categoria e Produto;

--- models/models.py
+++ models/models.py
@@ -2,6 +2,19 @@ class Categoria:
     def __init__(self, nome, descricao):
         self.nome = nome
         self.descricao = descricao
+    
+
+    def criar(self):
+        return 0
+
+
+    def remover(self):
+        return 0
+
+    
+    def listar(self):
+        return 0
+
 
 class Produto:
     def __init__(self, nome, valor, descricao, categoria):
@@ -9,3 +22,15 @@ class Produto:
         self.descricao = descricao
         self.valor = valor
         self.categoria = categoria
+
+    
+    def criar(self):
+        return 0
+
+
+    def remover(self):
+        return 0
+
+    
+    def listar(self):
+        return 0
