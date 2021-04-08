84c5844ef2d8d270a64d156b90e4529c8528c1fa
danilovbarbosa <danilo.v.barbosa@gmail.com>
2021-04-08 17:17:22 -0300

    refactor(menu): funções para inserir categoria e produto.

--- models_dao/menu.py
+++ models_dao/menu.py
@@ -2,6 +2,15 @@ from .models import *
 from .dao import *
 
 
+categoria_dao, produto_dao = inicializar_arquivos()
+
+
+def inicializar_arquivos():
+    categoria_dao = CategoriaDAO("categorias.txt")
+    produto_dao = ProdutoDAO("produto.txt")
+    return categoria_dao, produto_dao
+
+
 def exibir_menu():
     print(
         '''
@@ -21,16 +30,35 @@ def escolher_opcao():
 
     if opcao == "0":
         exit(1)
+
     elif opcao == "1":
-        categoria = Categoria("NOmecategoria", "Descricao batete")
-        categoria_dao = CategoriaDAO("categorias.txt")
-        categoria_dao.inserir(categoria.__str__())
-        print(categoria_dao.listar())
+        inserir_categoria()
+
     elif opcao == "2":
-        pass
+        inserir_produto()
+
     elif opcao == "3":
-        pass
+        print(categoria_dao.listar())
+
     elif opcao == "4":
-        pass
+        print(produto_dao.listar())
     else:
         print("Opção escolhida não corresponde as possibilidades indicadas.")
+
+
+def inserir_categoria():
+    nome = str(input("Digite o nome da categoria: "))
+    descricao = str(input("Digite o descrição da categoria: "))
+
+    categoria = Categoria(nome, descricao)
+    categoria_dao.inserir(categoria.__str__())
+
+
+def inserir_produto():
+    nome = str(input("Digite o nome da categoria: "))
+    valor = str(input("Digite a valor do produto: "))
+    descricao = str(input("Digite a descição do produto: "))
+    categoria = str(input("Digite o nome de uma categoria já registrada: "))
+
+    produto = Produto(nome, valor, descricao, categoria)
+    produto_dao.inserir(categoria.__str__())
