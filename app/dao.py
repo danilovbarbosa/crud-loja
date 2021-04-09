c3285f89186a6b9c61aeb3d80dbc7ace60464b8f
danylagacione <danielycrys24@gmail.com>
2021-04-09 16:21:40 -0300

    add metodos para usar banco de dados

--- app/dao.py
+++ app/dao.py
@@ -1,5 +1,6 @@
 from .models import Categoria, Produto
 from .util import Arquivo  
+import sqlite3
 
 
 class CategoriaDAO:
@@ -8,8 +9,19 @@ class CategoriaDAO:
         Arquivo.criar(nome_do_arquivo)
 
 
-    def inserir(self, categoria): 
-        Arquivo.escrever(self.nome_do_arquivo, categoria)
+    # def inserir(self, categoria): 
+    #     Arquivo.escrever(self.nome_do_arquivo, categoria)
+
+
+    def inserir_dados_categoria(self, categoria):
+        try:
+            self.db.cursor.execute(f"""INSERTO INTO Category (nome, descricao)
+            VALUES('{nome}', '{descricao}'))""")
+            self.db.commit()
+            print("Dados inseridos com sucesso !")
+        except sqlite3.IntegrityError:
+            print("Ocorreu um erro ao inserir dados")  
+            return False    
 
 
     def remover(self):
@@ -19,9 +31,14 @@ class CategoriaDAO:
         return 0
 
 
-    def listar(self):
-        categorias = Arquivo.ler(self.nome_do_arquivo).splitlines()
-        return categorias
+    # def listar(self):
+    #     categorias = Arquivo.ler(self.nome_do_arquivo).splitlines()
+    #     return categorias
+
+
+    def listar_categorias(self):
+            categoria = self.db.cursor.execute(f""" SELECT * FROM Category""")
+            return categoria.fetchall()    
 
 
     @staticmethod
@@ -41,8 +58,19 @@ class ProdutoDAO:
         Arquivo.criar(nome_do_arquivo)
 
 
-    def inserir(self, produto: Produto):
-        Arquivo.escrever(self.nome_do_arquivo, produto)
+    # def inserir(self, produto: Produto):
+    #     Arquivo.escrever(self.nome_do_arquivo, produto)
+
+
+    def inserir_dados_produtos(self, produto):
+        try:
+            self.db.cursor.execute(f"""INSERTO INTO Product (nome, valor, descricao, categoria)
+            VALUES('{nome}', '{valor}', '{descricao}', '{categoria}'))""")
+            self.db.commit()
+            print("Produto inserido com sucesso !")
+        except sqlite3.IntegrityError:
+            print("Ocorreu um erro ao inserir dados do produto")  
+            return False        
 
 
     def remover(self):
@@ -52,7 +80,11 @@ class ProdutoDAO:
         return 0
 
 
-    def listar(self):
-        produtos = Arquivo.ler(self.nome_do_arquivo).splitlines()
+    # def listar(self):
+    #     produtos = Arquivo.ler(self.nome_do_arquivo).splitlines()
+    #     return produtos
 
-        return produtos
+
+    def listar_produtos(self):
+            produto = self.db.cursor.execute(f""" SELECT * FROM Product""")
+            return produto.fetchall()             
